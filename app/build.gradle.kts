35c97267a9cb4754c5332773a8f0109411d766a0
matheussouza <mmedeiros.cgp@gmail.com>
2024-11-15 20:54:08 -0300

    implementação de dependencia do retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.koin)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.jaxb)
 }
