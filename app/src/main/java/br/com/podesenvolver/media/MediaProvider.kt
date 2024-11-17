07b88959f89d98cdbe1106f4254489b4bef400eb
Matheus Souza <mmedeiros.cbp@gmail.com>
2024-11-17 17:25:41 -0300

    ajuste de buffering do exo player para conter glitch ao reproduzir o audio

--- app/src/main/java/br/com/podesenvolver/media/MediaProvider.kt
+++ app/src/main/java/br/com/podesenvolver/media/MediaProvider.kt
@@ -1,10 +1,23 @@
 package br.com.podesenvolver.media
 
 import android.app.Application
+import androidx.annotation.OptIn
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.DefaultLoadControl
 import androidx.media3.exoplayer.ExoPlayer
 
 class MediaProvider {
     companion object {
-        fun createExoPlayer(application: Application) = ExoPlayer.Builder(application).build()
+        @OptIn(UnstableApi::class)
+        fun createExoPlayer(application: Application): ExoPlayer {
+            val loadControl = DefaultLoadControl.Builder()
+                .setBufferDurationsMs(
+                    DefaultLoadControl.DEFAULT_MIN_BUFFER_MS,
+                    DefaultLoadControl.DEFAULT_MAX_BUFFER_MS,
+                    DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_MS,
+                    DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS)
+                .build();
+            return ExoPlayer.Builder(application).setLoadControl(loadControl).build()
+        }
     }
 }
