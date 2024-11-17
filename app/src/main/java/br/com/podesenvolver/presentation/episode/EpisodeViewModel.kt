9b4d2662b14ccf5e9f056465976127caa0636df1
Matheus Souza <mmedeiros.cbp@gmail.com>
2024-11-17 16:27:57 -0300

    uso de StateFlow no EpisodeViewModel.kt

--- app/src/main/java/br/com/podesenvolver/presentation/episode/EpisodeViewModel.kt
+++ app/src/main/java/br/com/podesenvolver/presentation/episode/EpisodeViewModel.kt
@@ -1,16 +1,16 @@
 package br.com.podesenvolver.presentation.episode
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.podesenvolver.data.local.repository.LocalPodcastRepository
 import br.com.podesenvolver.domain.Episode
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class EpisodeViewModel(private val repository: LocalPodcastRepository) : ViewModel() {
-    private val _episode = MutableLiveData<Event>(Event.Loading)
-    val episode: LiveData<Event> = _episode
+    private val _episode = MutableStateFlow<Event>(Event.Loading)
+    val episode: StateFlow<Event> = _episode
 
     fun fetchEpisodeDataById(id: String) {
         _episode.value = Event.Loading
